92a7a076998fd1dcfef3c0232d60e89919105a4d
yangxioaping <yangxiaoping@westcoast.com.cn>
2017-02-28 14:05:04 +0800

    1111

---
+++ RecyclerView/recyclerview/src/main/java/uda/jwdf/com/recyclerview/Test.java
@@ -0,0 +1,10 @@
+package uda.jwdf.com.recyclerview;
+
+/**
+ * Created by Administrator on 2017/2/28.
+ */
+public class Test {
+    public double addition(double addnum1,double addnum2){
+        return addnum1+addnum2;
+    }
+}
